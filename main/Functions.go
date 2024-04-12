4bb844b3eb4e6bd7fafe91be1cf665c7869a99ef
QuiD-0 <wodnd101@naver.com>
2024-04-12 11:01:44 +0900

    function

---
+++ main/Functions.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := multiReturns(5, "Hello")
+	fmt.Println(a, b)
+
+	c, d := swap(5, 10)
+	fmt.Println(c, d)
+
+	fmt.Println(recursive(5))
+
+	dfs(3, "")
+	fmt.Println(arr)
+}
+
+func swap(a int, b int) (x int, y int) {
+	x = b
+	y = a
+	return
+}
+
+func multiReturns(x int, y string) (result int, txt1 string) {
+	result = x + x
+	txt1 = y + " World!"
+	return
+}
+
+func recursive(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * recursive(n-1)
+}
+
+var arr []string
+
+func dfs(n int, word string) {
+	if n == 0 {
+		arr = append(arr, word)
+		return
+	}
+	for i := 0; i < 4; i++ {
+		var next = word + string(rune('a'+i))
+		dfs(n-1, next)
+	}
+}
